16b6682abac908b3a0d920e50a27f1e0233e7c2c
Everton Pavan <everton.pavan.dev@gmail.com>
2023-01-16 16:56:14 -0300

    feat: add modal

---
+++ src/components/ModalData/index.tsx
@@ -0,0 +1,46 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    Button
+  } from '@chakra-ui/react'
+
+interface IModalProps {
+  data: string[];
+}
+
+function ModalData({
+  data
+}: IModalProps) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+      <>
+        <Button onClick={onOpen}>Open Modal</Button>
+  
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button variant='ghost'>Secondary Action</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
+
+  export { ModalData } ;
